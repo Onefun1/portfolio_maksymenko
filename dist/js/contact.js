3a286702dc528f8b1ae9b676b5bf7bbf3d3fbe59
Maksym Maksymenko <finside41@gmail.com>
2019-10-24 12:27:54 +0300

    add some changes cv fix

---
+++ dist/js/contact.js
@@ -0,0 +1,22 @@
+// Download all
+
+// (function() {
+//   const links = ["./CV_Maksymenko_Maksym.pdf", "./CV_Maksymenko_Maksym.docx"];
+//   const div = document.querySelector(".cv-block");
+//   let link = document.createElement("a");
+//   let span = document.createElement("span");
+//   span.classList.add("text-secondary");
+//   span.innerText = "Download";
+//   link.setAttribute("download", "CV_Maksymenko_Maksym");
+//   link.innerText = "CV (*.pdf, *.docx) ";
+//   link.appendChild(span);
+//   div.appendChild(link);
+
+//   function downloadAll(urls) {
+//     for (let i = 0; i < urls.length; i++) {
+//       link.setAttribute("href", urls[i]);
+//       link.click();
+//     }
+//   }
+//   div.addEventListener("click", () => downloadAll(links));
+// })();
